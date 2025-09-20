1a5e276824ef73881cd2c9324de1055e32f084b1
Oshen Dikkumbura <oshen@echonlabs.com>
2025-09-20 11:21:21 +0530

    add dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LightColors } from './themes/colors';
+import { API_URL } from '@env';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -16,6 +17,7 @@ function App() {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.container}>
         <Text>Hello</Text>
+        <Text>{API_URL}</Text>
       </View>
     </SafeAreaProvider>
   );
